8a2d932c15911eea0a85053e25c95894af67786c
Shane Kleinbeck <shanekleinbeck@gmail.com>
2017-02-13 16:04:48 -0500

    Maintenance

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 var express = require('express');
 var app = express();
+var request = require('request');
 
 app.use(express.static(__dirname + '/'));
 
 app.route('/')
-    .get(function(req, res){
-        // res.render('index.html');
-        var url = "https://api.darksky.net/forecast/379c15beaf1957c0afaca05bd48f9423/37.8267,-122.4233";
+  .get(function(req, res){
+    var url = "https://api.darksky.net/forecast/379c15beaf1957c0afaca05bd48f9423/37.8267,-122.4233";
 
-        request(url, function(error, response, body){
-            if(!error && response.statusCode == 200){
-                var data = JSON.parse(body);
-                res.render('index', {data: data});
-            } else {
-                console.log(error);
-                console.log(response.statusCode);
-            }
-        })
-    });
+		request(url, function(error, response, body){
+	    if(!error && response.statusCode == 200){
+	      var data = JSON.parse(body);
+	      res.render('index.ejs', {data: data});
+	    } else {
+	      console.log(error);
+	      console.log(response.statusCode);
+	    }
+	  })
+
+
+  // End .Get 
+	});
 
 app.listen(process.env.PORT || 8000);
 console.log('CONNECTED');
