4d7f14443786e9d41da3e6b77a67d98bb132ce54
Shane Kleinbeck <shanekleinbeck@gmail.com>
2017-02-13 15:04:42 -0500

    Completed Bottom Forecast HTML and CSS

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ app.use(express.static(__dirname + '/'));
 
 app.route('/')
     .get(function(req, res){
-        res.render('index.html');
+        // res.render('index.html');
+        var url = "https://api.darksky.net/forecast/379c15beaf1957c0afaca05bd48f9423/37.8267,-122.4233";
+
+        request(url, function(error, response, body){
+            if(!error && response.statusCode == 200){
+                var data = JSON.parse(body);
+                res.render('index', {data: data});
+            } else {
+                console.log(error);
+                console.log(response.statusCode);
+            }
+        })
     });
 
 app.listen(process.env.PORT || 8000);
