af2e3f98f9f808b244550cbe48cfa0ceb7c8f9ad
Shane Kleinbeck <shanekleinbeck@gmail.com>
2017-02-13 23:13:42 -0500

    Initial Work on API data

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 var express = require('express');
 var app = express();
 var request = require('request');
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/'));
 
 app.route('/')
   .get(function(req, res){
+
+		// ******  Set DarkSky API URL  ******
     var url = "https://api.darksky.net/forecast/379c15beaf1957c0afaca05bd48f9423/37.8267,-122.4233";
 
+		// ******  Retrieve Data  ******
 		request(url, function(error, response, body){
 	    if(!error && response.statusCode == 200){
+				// ******  Parse To String  ******
 	      var data = JSON.parse(body);
 	      res.render('index.ejs', {data: data});
 	    } else {
 	      console.log(error);
 	      console.log(response.statusCode);
 	    }
-	  })
+	  });
+
+
 
   // End .Get 
 	});
