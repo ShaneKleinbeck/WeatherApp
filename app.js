0075563354f1a964b8eccca34521e9237d9fb0d6
Shane Kleinbeck <shanekleinbeck@gmail.com>
2017-02-24 18:49:18 -0500

    Added Second Request to receive Lat and Lng from gogole API

--- app.js
+++ app.js
@@ -7,75 +7,82 @@ var moment = require('moment');
 // Local Imports
 var weatherIcon = require('./public/js/weather-icon.js');
 
-// Variable Declarations
-var data = null;
-var currentIcon = null;
-var currentTemp = null;
-var lowTemp = null;
-var currentWind = null;
-var currentDay = null;
-var remainingDays = null;
-var remainingIcons = null;
-
 // Set Middleware For Static Files
 app.use(express.static(__dirname + '/'));
 
 // Main Route
 app.route('/')
   	.get(function(req, res){
-		// Request API JSON From Dark Sky
-	  	var url = 'https://api.darksky.net/forecast/379c15beaf1957c0afaca05bd48f9423/37.8267,-122.4233';
-		request(url, function(error, response, body){
+
+		// Request API JSON From Google
+		var locationUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=davie&key=AIzaSyBmhUeyk5R5TZ9rPxmRkjY5znzAdejPNCk`;
+		request(locationUrl, function(error, response, body){
 			if(!error && response.statusCode === 200){
+				var locationData = JSON.parse(body);
+				var lat = locationData['results'][0]['geometry']['location']['lat'];
+				var lng = locationData['results'][0]['geometry']['location']['lng'];
+				var latlng = lat + ',' + lng;
 
-				// Variable Assignments
-				var data = JSON.parse(body);
-				var currentIcon = weatherIcon.pickIcon(data['currently']['icon']);
-				var currentTemp = Math.round(data['currently']['temperature']);
-				var apparentTemp = Math.round(data['currently']['apparentTemperature']);
-				var currentWind = Math.round(data['currently']['windSpeed']);
-				var currentDay = moment.unix(data['currently']['time']).format('dddd, Do');
-				var remainingDays = {
-					1: { 
-						'day': moment.unix(data['daily']['data'][1]['time']).format('dddd'),
-						'icon': weatherIcon.pickIcon(data['daily']['data'][1]['icon']),
-						'temp': Math.round(data['daily']['data'][1]['temperatureMax']) },
-					2: {
-						'day': moment.unix(data['daily']['data'][2]['time']).format('dddd'),
-						'icon': weatherIcon.pickIcon(data['daily']['data'][2]['icon']),
-						'temp': Math.round(data['daily']['data'][2]['temperatureMax']) },
-					3: {
-						'day': moment.unix(data['daily']['data'][3]['time']).format('dddd'),
-						'icon': weatherIcon.pickIcon(data['daily']['data'][3]['icon']),
-						'temp': Math.round(data['daily']['data'][3]['temperatureMax']) },
-					4: {
-						'day': moment.unix(data['daily']['data'][4]['time']).format('dddd'),
-						'icon': weatherIcon.pickIcon(data['daily']['data'][4]['icon']),
-						'temp': Math.round(data['daily']['data'][4]['temperatureMax']) },
-					5: {
-						'day': moment.unix(data['daily']['data'][5]['time']).format('dddd'),
-						'icon': weatherIcon.pickIcon(data['daily']['data'][5]['icon']),
-						'temp': Math.round(data['daily']['data'][5]['temperatureMax']) },
-					6: {
-						'day': moment.unix(data['daily']['data'][6]['time']).format('dddd'),
-						'icon': weatherIcon.pickIcon(data['daily']['data'][6]['icon']),
-						'temp': Math.round(data['daily']['data'][6]['temperatureMax'])
-					}
-				};
+				// Request API JSON From Dark Sky
+				var url = `https://api.darksky.net/forecast/379c15beaf1957c0afaca05bd48f9423/${latlng}`;
+				request(url, function(error, response, body){
+					if(!error && response.statusCode === 200){
+						// Variable Assignments
+						var data = JSON.parse(body);
+						var currentIcon = weatherIcon.pickIcon(data['currently']['icon']);
+						var currentTemp = Math.round(data['currently']['temperature']);
+						var apparentTemp = Math.round(data['currently']['apparentTemperature']);
+						var currentWind = Math.round(data['currently']['windSpeed']);
+						var currentDay = moment.unix(data['currently']['time']).format('dddd, Do');
+						var remainingDays = {
+							1: { 
+								'day': moment.unix(data['daily']['data'][1]['time']).format('dddd'),
+								'icon': weatherIcon.pickIcon(data['daily']['data'][1]['icon']),
+								'temp': Math.round(data['daily']['data'][1]['temperatureMax']) },
+							2: {
+								'day': moment.unix(data['daily']['data'][2]['time']).format('dddd'),
+								'icon': weatherIcon.pickIcon(data['daily']['data'][2]['icon']),
+								'temp': Math.round(data['daily']['data'][2]['temperatureMax']) },
+							3: {
+								'day': moment.unix(data['daily']['data'][3]['time']).format('dddd'),
+								'icon': weatherIcon.pickIcon(data['daily']['data'][3]['icon']),
+								'temp': Math.round(data['daily']['data'][3]['temperatureMax']) },
+							4: {
+								'day': moment.unix(data['daily']['data'][4]['time']).format('dddd'),
+								'icon': weatherIcon.pickIcon(data['daily']['data'][4]['icon']),
+								'temp': Math.round(data['daily']['data'][4]['temperatureMax']) },
+							5: {
+								'day': moment.unix(data['daily']['data'][5]['time']).format('dddd'),
+								'icon': weatherIcon.pickIcon(data['daily']['data'][5]['icon']),
+								'temp': Math.round(data['daily']['data'][5]['temperatureMax']) },
+							6: {
+								'day': moment.unix(data['daily']['data'][6]['time']).format('dddd'),
+								'icon': weatherIcon.pickIcon(data['daily']['data'][6]['icon']),
+								'temp': Math.round(data['daily']['data'][6]['temperatureMax'])
+							}
+						};
 
-				// Render Data to Index
-				res.render('index.ejs', { 
-					currentIcon: currentIcon,
-					currentTemp: currentTemp,
-					apparentTemp: apparentTemp,
-					currentWind: currentWind,
-					currentDay: currentDay,
-					remainingDays: remainingDays
+						// Render Data to Index
+						res.render('index.ejs', { 
+							currentIcon: currentIcon,
+							currentTemp: currentTemp,
+							apparentTemp: apparentTemp,
+							currentWind: currentWind,
+							currentDay: currentDay,
+							remainingDays: remainingDays
+						});
+					} else {
+						console.log(response.statusCode);
+						console.log(error);
+					};
 				});
+				// End Dark Sky Request
 			} else {
+				console.log(response.statusCode);
 				console.log(error);
-			}
+			};
 		});
+		// End Google Request
 	});
 
 app.listen(process.env.PORT || 8000);
